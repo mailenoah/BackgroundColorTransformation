c76aad9bb1bef38b2fb5d8db265ee37ba9b3b124
disegomaile1980 <u21477729@tuks.co.za>
2025-04-13 03:48:05 +0200

    Add project files.

---
+++ EXER 1a/Form1.Designer.cs
@@ -0,0 +1,76 @@
+﻿
+namespace EXER_1a
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnChangeToBlue = new System.Windows.Forms.Button();
+            this.btnYellow = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // btnChangeToBlue
+            // 
+            this.btnChangeToBlue.Location = new System.Drawing.Point(279, 102);
+            this.btnChangeToBlue.Name = "btnChangeToBlue";
+            this.btnChangeToBlue.Size = new System.Drawing.Size(98, 53);
+            this.btnChangeToBlue.TabIndex = 0;
+            this.btnChangeToBlue.Text = "Blue";
+            this.btnChangeToBlue.UseVisualStyleBackColor = true;
+            this.btnChangeToBlue.Click += new System.EventHandler(this.btnChangeToBlue_Click);
+            // 
+            // btnYellow
+            // 
+            this.btnYellow.Location = new System.Drawing.Point(279, 229);
+            this.btnYellow.Name = "btnYellow";
+            this.btnYellow.Size = new System.Drawing.Size(98, 44);
+            this.btnYellow.TabIndex = 1;
+            this.btnYellow.Text = "Yellow";
+            this.btnYellow.UseVisualStyleBackColor = true;
+            this.btnYellow.Click += new System.EventHandler(this.btnYellow_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.btnYellow);
+            this.Controls.Add(this.btnChangeToBlue);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnChangeToBlue;
+        private System.Windows.Forms.Button btnYellow;
+    }
+}
+
---
+++ EXER 1a/Form1.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace EXER_1a
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnChangeToBlue_Click(object sender, EventArgs e)
+        {
+            this.BackColor = Color.Blue;
+        }
+
+        private void btnYellow_Click(object sender, EventArgs e)
+        {
+            this.BackColor = Color.Yellow;
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
